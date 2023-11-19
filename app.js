b24f9cdf7b8f62a26ba808224eefdd2f59f69f40
EphremG <fstudioceh@gmail.com>
2023-11-19 11:02:35 -0600

    update

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 const mysql = require('mysql');
 const path = require('path');
 const app = express();
+var ejs = require("ejs");
 
 // parse application/json
 app.use(bodyParser.json());
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser())
+//EJS Engine
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
 
 //create connection
 const db = mysql.createConnection({
@@ -28,30 +35,37 @@ db.connect((err) => {
     console.log('Mysql Connect');
 })
 
-
-
 app.listen(8000, () => {
     console.log('Server started on port 8000')
 })
 
+let display = false;
 
 
 app.get("/",(req,res) =>{
-    res.sendFile(path.join(__dirname,'/pages/addQuestion.html'))
+    //res.sendFile(path.join(__dirname,'/views/showQuestions.html'))
+    res.render('showQuestions',{display})
 })
 
+app.get("/createquiz",(req,res) =>{
+    res.sendFile(path.join(__dirname,'/views/createQuiz.html'))
+})
+
+
 //Insert hostInfo
-app.get('/hostInfo', (req,res) => {
+app.post('/hostInfo', (req,res) => {
     
-    //let title = req.body.title;
-   // let HostID = res.body.HostID;
+    let title = req.body.title;
+    let HostID = Math.floor(Math.random() * 90000) + 10000;
 
-
-    let data = {HostID:'12345', Title:'Title1'};
+    let data = {HostID:HostID, Title:title};
     let sql = 'insert into HostInfo SET ?';
     let query = db.query(sql, data, (err, result) => {
         if(!err){
-            res.send(result)
+            res.cookie('HostID', HostID)
+            res.cookie('Title', title)
+            res.redirect('/')
+            //res.render('showQuestions',{Title: title, HostID:HostID})
         }
     })
     // console.log(query);
@@ -60,13 +74,15 @@ app.get('/hostInfo', (req,res) => {
 //insert question
 
 app.post('/addQuestion', (req,res) => {
+    display = true;
+
     let Question = req.body.qn;
     let Option1 = req.body.Option1;
     let Option2 = req.body.Option2;
     let Option3 = req.body.Option3;
     let Option4 = req.body.Option4;
     let CorrectAnswer = req.body.CorrectAnswer;
-   // let HostID = res.body.HostID;
+    let HostID = req.cookies.HostID;
 
   // console.log(Question)
 
@@ -74,23 +90,27 @@ app.post('/addQuestion', (req,res) => {
     let sql = 'insert into questionBank SET ?';
     let query = db.query(sql, data, (err, result) => {
         if(!err){
-            res.send(result)
+           res.redirect('/showQuestions')
         }
-        //throw err;
     })
-    // console.log(query);
 })
 
-app.get('/showQuestions', (req,res) => {
-    let sql = 'select * from questionBank';
+
+app.get('/showQuestions', (req, res) => {
+    let sql = 'SELECT * FROM questionBank';
     let query = db.query(sql, (err, result) => {
         if(!err){
-            res.send(result)
+           res.render('showQuestions', {questions: result, display});
         }
-    })
-    console.log(query);
-})
+    });
+});
 
+
+app.get('/joinhost', (req, res) => {
+    let code = req.cookies.HostID;
+    console.log(code);
+    res.render('join',{code})
+});
 
 
 
